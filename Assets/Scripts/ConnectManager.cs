dd07d0adbcbac53c58b73775434216902a46f9c1
DESKTOP-KCNL1IF\Rakotovao Romane <romanerakotovao@gmail.com>
2017-12-16 16:22:52 +0100

    Bimboum c'était juste la scène de base et la variable

--- Assets/Scripts/ConnectManager.cs
+++ Assets/Scripts/ConnectManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ConnectManager : MonoBehaviour {
-
+	public bool HandHolding = false;
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +11,10 @@ public class ConnectManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetKey (KeyCode.Space)) {
+			HandHolding = true;
+		} else {
+			HandHolding = false;
+		}
 	}
 }
